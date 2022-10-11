d850b6c76c41409e4730fadc6df8d984745cec33
Saba <Saba@DESKTOP-UH0LFB7>
2022-10-11 14:04:18 -0700

    Add project files.

---
+++ father/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace father
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            Console.WriteLine("Enter your Choice :\n 1- Dollar to Rupee \n  2 - Euro to Rupee \n 3 - Malaysian Ringgit to Rupee ");
+            choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Double dollar, rupee, val;
+                    Console.WriteLine("Enter the Dollar Amount :");
+                    dollar = Double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the Dollar Value :");
+                    val = double.Parse(Console.ReadLine());
+                    rupee = dollar * val;
+                    Console.WriteLine("{0} Dollar Equals {1} Rupees", dollar, rupee);
+                    break;
+                case 2:
+                    Double Euro, rupe, valu;
+                    Console.WriteLine("Enter the Euro Amount :");
+                    Euro = Double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the Euro Value :");
+                    valu = double.Parse(Console.ReadLine());
+                    rupe = Euro * valu;
+                    Console.WriteLine("{0} Euro Equals {1} Rupees", Euro, rupe);
+                    break;
+                case 3:
+                    Double ringit, rup, value;
+                    Console.WriteLine("Enter the Ringgit Amount :");
+                    ringit = Double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the Ringgit Value :");
+                    value = double.Parse(Console.ReadLine());
+                    rup = ringit * value;
+                    Console.WriteLine("{0} Malaysian Ringgit Equals {1} Rupees",
+                                      ringit, rup);
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
+        
+   
+
